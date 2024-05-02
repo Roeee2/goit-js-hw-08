d8e285a4c1d8a194462945cb4c98052351e54ddc
Roeee2 <122810145+Roeee2@users.noreply.github.com>
2024-05-02 21:41:09 +0100

    task2-done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe#vimeo-player');
+const player = new Player(iframe);
+const currentTime = local.Storage.getItem('currentTime');
+
+if (currentTIme) {
+  player.setCurrentTime(Number.parseFloat(currentTime));
+}
+
+player.on('timeupdate', data => {
+  localStorage.setItem('currentTime', data.seconds);
+});
