a96eec3212ddbbe95a979465a31b5d1fa25f5575
Roeee2 <122810145+Roeee2@users.noreply.github.com>
2024-05-02 20:53:33 +0100

    small-change

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,6 @@
 // Add imports above this line
 import SimpleLightbox from 'simplelightbox';
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Change code below this line
