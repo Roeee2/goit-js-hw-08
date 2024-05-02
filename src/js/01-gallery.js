96c0b368211c76ec3e1d1c37941e9cddc4e8b143
Roeee2 <122810145+Roeee2@users.noreply.github.com>
2024-05-02 20:45:25 +0100

    task1-done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+for (const item of galleryItems) {
+  gallery.insertAdjacentHTML(
+    'beforeend',
+    `<li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>`
+  );
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
